d66209cf27d5d4c6be699f64a8944c73080a97f3
Thomas Chevron <chevron.thomas33@gmail.com>
2023-10-06 18:20:56 +0200

    Fix Bills.js closed

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,13 +2,16 @@
  * @jest-environment jsdom
  */
 import "@testing-library/jest-dom";
-import { screen, waitFor } from "@testing-library/dom";
+import { fireEvent, screen, waitFor } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
 import { ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
-
+import mockStore from "../__mocks__/store";
+import Bills from "../containers/Bills.js";
 import router from "../app/Router.js";
+jest.mock("../app/store", () => mockStore)
+
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -59,5 +62,91 @@ describe("Given I am connected as an employee", () => {
       // et que le code a fonctionné comme prévu
       expect(allDatesPresent).toBe(true);
     });
+
+
+    // Test pour les notes de frais
+    test("then I create a new expense report", () =>{
+      const mockOnNavigate = jest.fn();
+      const mockBills = new Bills({
+        document: document,
+        onNavigate: mockOnNavigate,
+        store: null,
+        localStorage: null
+      });
+      document.body.innerHTML = '<button type="button" data-testid="btn-new-bill" class="btn btn-primary">Nouvelle note de frais</button>';
+      mockBills.handleClickNewBill();
+      expect(mockOnNavigate).toHaveBeenCalledWith(ROUTES_PATH.NewBill);
+    });
+
+    // Test de l'affichage d'un fichier en cliquant sur l'icone oeil
+    test("then I click on the eye icon to display a file", () =>{
+      document.body.innerHTML = BillsUI({data: bills});
+
+      const mockClick = new Bills({
+        document,
+        onNavigate,
+        store: null,
+        localStorage: window.localStorage
+      });
+      
+      const modale = document.getElementById('modaleFile');
+      $.fn.modal = jest.fn(()=> modale.classList.add('show'));
+      const iconEye = screen.getAllByTestId('icon-eye')[0];
+
+      const handleClickIconEye = jest.fn(mockClick.handleClickIconEye(iconEye));
+      iconEye.addEventListener('click', handleClickIconEye());
+      fireEvent.click(iconEye);
+
+      expect(handleClickIconEye).toHaveBeenCalled();
+    });
   });
+
+  // Test d'intégration de la méthode GET pour GETBills
+  describe("When an error occurs on API", () => {
+    beforeEach(() => {
+      jest.spyOn(mockStore, "bills")
+      Object.defineProperty(
+          window,
+          'localStorage',
+          { value: localStorageMock }
+      )
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee',
+        email: "a@a"
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.appendChild(root)
+      router()
+    })
+    test("fetches bills from an API and fails with 404 message error", async () => {
+
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          list : () =>  {
+            return Promise.reject(new Error("Erreur 404"))
+          }
+        }})
+      window.onNavigate(ROUTES_PATH.Dashboard)
+      await new Promise(process.nextTick);
+      const message = await screen.getByText(/Erreur 404/)
+      expect(message).toBeTruthy()
+    })
+
+    test("fetches messages from an API and fails with 500 message error", async () => {
+
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          list : () =>  {
+            return Promise.reject(new Error("Erreur 500"))
+          }
+        }})
+
+      window.onNavigate(ROUTES_PATH.Dashboard)
+      await new Promise(process.nextTick);
+      const message = await screen.getByText(/Erreur 500/)
+      expect(message).toBeTruthy()
+    })
+  })
+
 });
--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -18,31 +18,26 @@ export default class NewBill {
     new Logout({ document, localStorage, onNavigate });
   }
   handleChangeFile = () => {
-    // e.preventDefault();
-    const file = this.document.querySelector(`input[data-testid="file"]`)
-      // .files[0];
-    // const filePath = e.target.value.split(/\\/g);
-
-    // const fileName = filePath[filePath.length - 1];
+    const file = this.document.querySelector(`input[data-testid="file"]`);
+    // Recupére le nom du fichier
     const fileName = file.files[0].name;
+    // Regex pour l'extension de fichiers
     const regexExtensionGranted = /\.(jpg|jpeg|png)$/i;
-    const errorMessage = this.document.querySelector('.error-message');
-
-    // Récupère l'extension du fichier
-    // const fileExtension = fileName.split(".").pop().toLowerCase();
+    // Sélectionne le message d'erreur
+    const errorMessage = this.document.querySelector(".error-message");
 
     // Vérifie si l'extension du fichier est autorisée
     if (!regexExtensionGranted.test(fileName)) {
-      
-      errorMessage.innerText = "Seuls les fichiers jpg, jpeg, et png sont autorisés.";
-      // Si l'extension du fichier n'est pas autorisée, affiche une alerte et retourne pour sortir de la fonction
+      // Si l'extension du fichier n'est pas autorisée, affiche un message d'erreur
+      errorMessage.innerText =
+        "Seuls les fichiers jpg, jpeg, et png sont autorisés.";
+      // Réinitialise le champ du fichier
       file.value = "";
-    } else{
+    } else {
+      // Supprime le message d'erreur si l'extension est valide
       errorMessage.innerText = "";
       return;
     }
-
-    
   };
   handleSubmit = (e) => {
     e.preventDefault();
@@ -71,7 +66,7 @@ export default class NewBill {
     };
 
     // Déplacement de 74 à 94 placé initialement dans hadleChangeFile vers handleSubmit
-    // Pour récupérer et mettre à jour les données au moment de l'emvoie et non au changement du fichier
+    // Pour récupérer et mettre à jour les données au moment de l'envoie et non au changement du fichier
     const formData = new FormData();
     formData.append("file", file);
     formData.append("email", email);
@@ -93,7 +88,6 @@ export default class NewBill {
         this.onNavigate(ROUTES_PATH["Bills"]);
       })
       .catch((error) => console.error(error));
-
   };
 
   // not need to cover this function by tests
