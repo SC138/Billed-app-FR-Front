7dff7a846c22843c4c42fc6b28257524e48d0d14
Thomas Chevron <chevron.thomas33@gmail.com>
2023-10-07 17:36:55 +0200

    adding comments

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -10,8 +10,7 @@ import { localStorageMock } from "../__mocks__/localStorage.js";
 import mockStore from "../__mocks__/store";
 import Bills from "../containers/Bills.js";
 import router from "../app/Router.js";
-jest.mock("../app/store", () => mockStore)
-
+jest.mock("../app/store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -63,40 +62,59 @@ describe("Given I am connected as an employee", () => {
       expect(allDatesPresent).toBe(true);
     });
 
-
     // Test pour les notes de frais
-    test("then I create a new expense report", () =>{
+    test("then I create a new expense report", () => {
+      // Creátaion d'une fonction mock pour simuler la navigation
       const mockOnNavigate = jest.fn();
+
+      // Création d'une instance mck de la class Bills
       const mockBills = new Bills({
         document: document,
         onNavigate: mockOnNavigate,
         store: null,
-        localStorage: null
+        localStorage: null,
       });
-      document.body.innerHTML = '<button type="button" data-testid="btn-new-bill" class="btn btn-primary">Nouvelle note de frais</button>';
+
+      // Simule le rendu d'un bouton dans le DOM
+      document.body.innerHTML =
+        '<button type="button" data-testid="btn-new-bill" class="btn btn-primary">Nouvelle note de frais</button>';
+      //Appelle la méthode handleClickNewBill() pour déclencher la navigation
       mockBills.handleClickNewBill();
+
+      // Vérifie si la fonction mockOnNavigate a été appelée avec le bon argument
+      // Ce qui indique que la navigation a bien été déclenchée
       expect(mockOnNavigate).toHaveBeenCalledWith(ROUTES_PATH.NewBill);
     });
 
     // Test de l'affichage d'un fichier en cliquant sur l'icone oeil
-    test("then I click on the eye icon to display a file", () =>{
-      document.body.innerHTML = BillsUI({data: bills});
+    test("then I click on the eye icon to display a file", () => {
+      // Simule le rendu de l'UI de Bills avec des données de factures
+      document.body.innerHTML = BillsUI({ data: bills });
 
+      // Création d'un nouvelle instance de la class Bills
       const mockClick = new Bills({
         document,
         onNavigate,
         store: null,
-        localStorage: window.localStorage
+        localStorage: window.localStorage,
       });
-      
-      const modale = document.getElementById('modaleFile');
-      $.fn.modal = jest.fn(()=> modale.classList.add('show'));
-      const iconEye = screen.getAllByTestId('icon-eye')[0];
 
+      // Récupération de l'élément modal du DOM
+      const modale = document.getElementById("modaleFile");
+      // Remplacement de la focntion $.fn.modal par une fonction fictive qui ajoute la class 'show' à la modale
+      $.fn.modal = jest.fn(() => modale.classList.add("show"));
+      // Récupération du premier élément avec l'icone oeil du DOM
+      const iconEye = screen.getAllByTestId("icon-eye")[0];
+
+      // Mock de la méthode handleClickIconEye de l'instance mockClick, et attacher cette simulation à l'icone oeil
       const handleClickIconEye = jest.fn(mockClick.handleClickIconEye(iconEye));
-      iconEye.addEventListener('click', handleClickIconEye());
+
+      // Écouteur d'event 'click' sur l'icone oeil qui appelle la méthode handleClickIconEye
+      iconEye.addEventListener("click", handleClickIconEye());
+      //Simulation d'un click sur l'icone oeil
       fireEvent.click(iconEye);
 
+      // Vérification que la méthode mockée (handleClickIconEye) a été appelée lors du click
       expect(handleClickIconEye).toHaveBeenCalled();
     });
   });
@@ -104,49 +122,49 @@ describe("Given I am connected as an employee", () => {
   // Test d'intégration de la méthode GET pour GETBills
   describe("When an error occurs on API", () => {
     beforeEach(() => {
-      jest.spyOn(mockStore, "bills")
-      Object.defineProperty(
-          window,
-          'localStorage',
-          { value: localStorageMock }
-      )
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee',
-        email: "a@a"
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.appendChild(root)
-      router()
-    })
+      jest.spyOn(mockStore, "bills");
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+          email: "a@a",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.appendChild(root);
+      router();
+    });
     test("fetches bills from an API and fails with 404 message error", async () => {
-
       mockStore.bills.mockImplementationOnce(() => {
         return {
-          list : () =>  {
-            return Promise.reject(new Error("Erreur 404"))
-          }
-        }})
-      window.onNavigate(ROUTES_PATH.Dashboard)
+          list: () => {
+            return Promise.reject(new Error("Erreur 404"));
+          },
+        };
+      });
+      window.onNavigate(ROUTES_PATH.Dashboard);
       await new Promise(process.nextTick);
-      const message = await screen.getByText(/Erreur 404/)
-      expect(message).toBeTruthy()
-    })
+      const message = await screen.getByText(/Erreur 404/);
+      expect(message).toBeTruthy();
+    });
 
     test("fetches messages from an API and fails with 500 message error", async () => {
-
       mockStore.bills.mockImplementationOnce(() => {
         return {
-          list : () =>  {
-            return Promise.reject(new Error("Erreur 500"))
-          }
-        }})
+          list: () => {
+            return Promise.reject(new Error("Erreur 500"));
+          },
+        };
+      });
 
-      window.onNavigate(ROUTES_PATH.Dashboard)
+      window.onNavigate(ROUTES_PATH.Dashboard);
       await new Promise(process.nextTick);
-      const message = await screen.getByText(/Erreur 500/)
-      expect(message).toBeTruthy()
-    })
-  })
-
+      const message = await screen.getByText(/Erreur 500/);
+      expect(message).toBeTruthy();
+    });
+  });
 });
